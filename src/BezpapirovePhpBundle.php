efd90170930b99bcf60bcefe285d34984613254c
Martin Koláček <martin@martinkolacek.cz>
2024-06-24 10:37:46 +0200

    update configuration

--- src/BezpapirovePhpBundle.php
+++ src/BezpapirovePhpBundle.php
@@ -4,8 +4,42 @@ declare(strict_types=1);
 
 namespace Bezpapirove\BezpapirovePhpBundle;
 
+use Bezpapirove\BezpapirovePhpLib\File\FileHandler;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class BezpapirovePhpBundle extends AbstractBundle
 {
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('file_handler')
+                    ->children()
+                        ->scalarNode('base_path')
+                            ->defaultValue('%kernel.project_dir%/public/runtime')
+                        ->end()
+                    ->end()
+                ->end() // file_handler
+            ->end()
+        ;
+    }
+
+    /**
+     * @param array|mixed[] $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../config/services.yaml');
+
+        if ($config['file_handler']['base_path']) {
+            $container->services()
+                ->get(FileHandler::class)
+                    ->arg(0, $config['file_handler']['base_path'])
+            ;
+        }
+    }
 }
